fd4d0c8d85b024201bc9e95d984eda384988787d
LampTales <179500516@qq.com>
2023-06-04 18:33:39 +0800

    regression, svm, imblearn

--- Project03/main.py
+++ Project03/main.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import torch
+import warnings
+
+# import tools
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import RandomOverSampler
+
+# import regression models
+from sklearn.linear_model import LogisticRegression
+
+# import svm models
+from sklearn import svm
+
+# import tree models
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from sklearn import tree
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
+
+def write_data_frame_to_file(data, file_name="test.txt"):
+    data.to_csv('data/' + file_name, index=False)
 
 
 def data_split(test_size, data, labels):
@@ -47,7 +65,8 @@ def self_pretreat(data, labels, print_info=False):
     # pd.set_option('display.max_columns', None)
     # print(data.dtypes)
     # print(data.nunique())
-    object_list = ['workclass', 'education', 'marital.status', 'occupation', 'relationship', 'race', 'sex', 'native.country']
+    object_list = ['workclass', 'education', 'marital.status', 'occupation', 'relationship', 'race', 'sex',
+                   'native.country']
     for obj in object_list:
         df_encoded = pd.get_dummies(data[obj], prefix=obj)
         data = pd.concat([data, df_encoded], axis=1)
@@ -59,51 +78,71 @@ def self_pretreat(data, labels, print_info=False):
     return data, labels
 
 
+def evaluate(y_test, y_pred, do_evaluation):
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    if do_evaluation:
+        print("accuracy: ", accuracy, "\tprecision: ", precision)
+    return accuracy
+
+
+# regression part
+def do_regression(X_train, X_test, y_train, y_test, do_evaluation=True):
+    warnings.filterwarnings("ignore")
+    clf = LogisticRegression()
+    clf.fit(X_train, y_train.values.ravel())
+
+    y_pred = clf.predict(X_test)
+    accuracy = evaluate(y_test, y_pred, do_evaluation)
+
+    return clf, accuracy
+
+
+# svm part
+def do_svm(X_train, X_test, y_train, y_test, do_evaluation=True):
+    clf = svm.SVC()
+    clf.fit(X_train, y_train.values.ravel())
+
+    y_pred = clf.predict(X_test)
+    accuracy = evaluate(y_test, y_pred, do_evaluation)
+
+    return clf, accuracy
+
+
 # tree part
 def do_tree(X_train, X_test, y_train, y_test, depth=7, print_tree=False, do_evaluation=True):
-    X = X_train
-    y = y_train
     clf = DecisionTreeClassifier(max_depth=depth)
-    clf.fit(X, y)
+    clf.fit(X_train, y_train)
 
     # show the tree
     if print_tree:
         text_representation = tree.export_text(clf)
         print(text_representation)
 
-    if do_evaluation:
-        y_pred = clf.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        print(accuracy)
+    y_pred = clf.predict(X_test)
+    accuracy = evaluate(y_test, y_pred, do_evaluation)
 
-    return clf
+    return clf, accuracy
+
 
 def do_forest(X_train, X_test, y_train, y_test, depth=None, estimators=100, do_evaluation=True):
-    X = X_train
-    y = y_train
     clf = RandomForestClassifier(max_depth=depth, n_estimators=estimators)
-    clf.fit(X, y.values.ravel())
+    clf.fit(X_train, y_train.values.ravel())
 
-    if do_evaluation:
-        y_pred = clf.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        print(accuracy)
+    y_pred = clf.predict(X_test)
+    accuracy = evaluate(y_test, y_pred, do_evaluation)
 
-    return clf
+    return clf, accuracy
 
 
 def do_xgboost(X_train, X_test, y_train, y_test, depth=6, estimators=100, learning_rate=0.3, do_evaluation=True):
-    X = X_train
-    y = y_train
     clf = XGBClassifier(max_depth=depth, n_estimators=estimators, learning_rate=learning_rate)
-    clf.fit(X, y)
+    clf.fit(X_train, y_train)
 
-    if do_evaluation:
-        y_pred = clf.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        print(accuracy)
+    y_pred = clf.predict(X_test)
+    accuracy = evaluate(y_test, y_pred, do_evaluation)
 
-    return clf
+    return clf, accuracy
 
 
 def do_net(X_train, X_test, y_train, y_test, do_evaluation=True):
@@ -111,22 +150,56 @@ def do_net(X_train, X_test, y_train, y_test, do_evaluation=True):
 
 
 def main():
+    use_self_encode = True
+    use_imblearn = False
+    use_smote = False
+
     data = pd.read_csv('data/traindata.csv')
     labels = pd.read_csv('data/trainlabel.txt')
-    # data, labels = sklearn_pretreat(data, labels)
-    data, labels = self_pretreat(data, labels)
 
+    # select data split
+    if use_self_encode:
+        data, labels = self_pretreat(data, labels)
+    else:
+        data, labels = sklearn_pretreat(data, labels)
     X_train, X_test, y_train, y_test = data_split(0.1, data, labels)
-    # clf = do_tree(X_train, X_test, y_train, y_test, depth=7, print_tree=False)
-    # clf = do_forest(X_train, X_test, y_train, y_test)
-    clf = do_xgboost(X_train, X_test, y_train, y_test)
 
-    # for i in range(20):
+    # deal data imbalance
+    if use_imblearn:
+        if use_smote:
+            X_train, y_train = SMOTE(random_state=42).fit_resample(X_train, y_train)
+        else:
+            X_train, y_train = RandomOverSampler(random_state=42).fit_resample(X_train, y_train)
+
+    # regression part
+    # clf, acc = do_regression(X_train, X_test, y_train, y_test)
+
+    # svm part
+    clf, acc = do_svm(X_train, X_test, y_train, y_test)
+
+    # tree part
+    # clf, acc = do_tree(X_train, X_test, y_train, y_test, depth=7, print_tree=False)
+    # clf, acc = do_forest(X_train, X_test, y_train, y_test)
+    # clf, acc = do_xgboost(X_train, X_test, y_train, y_test)
+
+    # multiple test
+    # avg_acc = 0
+    # for i in range(30):
     #     X_train, X_test, y_train, y_test = data_split(0.1, data, labels)
-    #     # clf = do_tree(X_train, X_test, y_train, y_test, depth=7, print_tree=False)
-    #     clf = do_forest(X_train, X_test, y_train, y_test, depth=None, estimators=100)
-
-
+    #     if use_imblearn:
+    #         if use_smote:
+    #             X_train, y_train = SMOTE(random_state=42).fit_resample(X_train, y_train)
+    #         else:
+    #             X_train, y_train = RandomOverSampler(random_state=42).fit_resample(X_train, y_train)
+    #
+    #     # clf, acc = do_regression(X_train, X_test, y_train, y_test)
+    #
+    #     # clf, acc = do_tree(X_train, X_test, y_train, y_test, depth=7, print_tree=False)
+    #     # clf, acc = do_forest(X_train, X_test, y_train, y_test, depth=None, estimators=100)
+    #     clf, acc = do_xgboost(X_train, X_test, y_train, y_test)
+    #
+    #     avg_acc += acc
+    # print("avg_acc: ", avg_acc / 30)
 
 
 if __name__ == "__main__":
